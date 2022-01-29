529c14230ce6b0790ce94e5860a5ef40629c7df2
Victor Leshkov <7065531+VLeshkov@users.noreply.github.com>
2022-01-29 11:09:05 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArray = str.split('');
+
+  for (let pair of bracketsConfig) {
+    if (bracketsArray[0] === pair[1] || bracketsArray.slice(-1) === pair[0]) {
+      return false;
+    }
+
+    const openCount = bracketsArray.filter(elem => elem === pair[0]).length;
+    const closerCount = bracketsArray.filter(elem => elem === pair[1]).length;
+
+    if (openCount != closerCount) {
+      return false;
+    }
+  }
+
+  return true;
 }
